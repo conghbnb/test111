c16c774220ba7ccb8cd508bc07723aac1309df54
Kong Lee <kong.lee@vinova.com.sg>
2024-06-04 14:33:30 +0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { SwapWidget } from "@uniswap/widgets";
+import "@uniswap/widgets/fonts.css";
+import "./App.css";
+
+const jsonRpcUrlMap = {
+  1: ["https://eth-pokt.nodies.app"],
+  56: ["https://bsc-pokt.nodies.app"],
+  8453: ["https://base-rpc.publicnode.com"],
+  // mạng nào nữa thì tự thêm vào đây, nó tự ăn..
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Uniswap">
+        <SwapWidget
+          tokenList={"https://ipfs.io/ipns/tokens.uniswap.org"}
+          // provider={provider}
+          jsonRpcUrlMap={jsonRpcUrlMap}
+        />
+      </div>
     </div>
   );
 }
